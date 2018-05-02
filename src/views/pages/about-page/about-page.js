ddafe19fd1ef82ce04a4d56ba9be38db7557d530
Michael Cassidy <mikecassidy0000@gmail.com>
2018-05-02 14:11:47 -0400

    about

--- src/views/pages/about-page/about-page.js
+++ src/views/pages/about-page/about-page.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import './about-page.css'
 
 export default class AboutPage extends Component {
   render() {
     return (
-      <div>  
+      <div className="about">
+        <div className="center">
+          <h1>This site was created to share some of my favorite mixes</h1>
+        </div>
       </div>
     )
   }
